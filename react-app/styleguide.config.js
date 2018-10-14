95eb1900099f81ff4d22822567a59647352f291a
Daniel Gordon <dgordon@adobe.com>
2018-10-14 09:37:59 -0400

    update

--- react-app/styleguide.config.js
+++ react-app/styleguide.config.js
@@ -1,10 +1,9 @@
 const path = require('path')
 module.exports = {
     components: 'src/components/**/[A-Z]*.js',
-    skipComponentsWithoutExample: true,
     assetsDir: 'public/images',
     require: [
         path.join(__dirname, 'src/index.scss')
       ],
-    ignore:  ['src/components/**/Page.js','src/components/**/MappedComponents.js','**/__tests__/**', '**/*.test.{js,jsx,ts,tsx}', '**/*.spec.{js,jsx,ts,tsx}', '**/*.d.ts']
+    ignore:  ['src/components/**/Page.js','src/components/**/MappedComponents.js','src/components/**/Header.js','**/__tests__/**', '**/*.test.{js,jsx,ts,tsx}', '**/*.spec.{js,jsx,ts,tsx}', '**/*.d.ts']
 }
