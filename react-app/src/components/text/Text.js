8d9347fefd1f047f59e0fdfb309469eb3ea923fc
Daniel Gordon <dgordon@adobe.com>
2018-10-16 20:52:20 -0400

    Chapter 1 Editable Components

---
+++ react-app/src/components/text/Text.js
@@ -0,0 +1,41 @@
+/*
+    Text.js
+
+    Maps to wknd-events/components/content/text
+*/
+
+import React, {Component} from 'react';
+import {MapTo} from '@adobe/cq-react-editable-components';
+/**
+ * Default Edit configuration for the Text component that interact with the Core Text component and sub-types
+ *
+ * @type EditConfig
+ */
+const TextEditConfig = {
+
+    emptyLabel: 'Text',
+
+    isEmpty: function(props) {
+        return !props || !props.text || props.text.trim().length < 1;
+    }
+};
+
+/**
+ * Text React component
+ */
+class Text extends Component {
+
+    get richTextContent() {
+        return <div dangerouslySetInnerHTML={{__html:  this.props.text}}/>;
+    }
+
+    get textContent() {
+        return <div>{this.props.text}</div>;
+    }
+
+    render() {
+        return this.props.richText ? this.richTextContent : this.textContent;
+    }
+}
+
+MapTo('wknd-events/components/content/text')(Text, TextEditConfig);
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ModelManager, Constants } from '@adobe/cq-spa-page-model-manager';
 import './index.css';
 import App from './App';
+import "./components/MappedComponents";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function render(model) {
+    ReactDOM.render((
+        <App cqChildren={ model[Constants.CHILDREN_PROP] } cqItems={ model[Constants.ITEMS_PROP] } cqItemsOrder={ model[Constants.ITEMS_ORDER_PROP] }
+            cqPath={ ModelManager.rootPath } locationPathname={ window.location.pathname }/>), document.getElementById('root'));
+}
+
+ModelManager.initialize({ path: process.env.REACT_APP_PAGE_MODEL_PATH }).then(render);
