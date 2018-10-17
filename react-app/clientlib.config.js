ac1b298e73859c6a277c74f9614f208e53d4875b
Daniel Gordon <dgordon@adobe.com>
2018-10-16 20:45:10 -0400

    updates for project setup (#3)

---
+++ react-app/clientlib.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // default working directory (can be changed per 'cwd' in every asset option)
+    context: __dirname,
+
+    // path to the clientlib root folder (output)
+    clientLibRoot: "./../ui.apps/src/main/content/jcr_root/apps/wknd-events/clientlibs",
+
+    libs: {
+        name: "react-app",
+        allowProxy: true,
+        categories: ["wknd-events.react"],
+        serializationFormat: "xml",
+        jsProcessor: ["min:gcc"],
+        assets: {
+            js: [
+                "build/static/**/*.js"
+            ],
+            css: [
+                "build/static/**/*.css"
+            ]
+        }
+    }
+};
