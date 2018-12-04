673a88f9ce54d9a9fbc79e57d1ca66c689221643
david g <davidjgonzalez@gmail.com>
2018-12-04 10:27:49 -0500

    Chapter 1 - End

---
+++ angular-app/clientlib.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // default working directory (can be changed per 'cwd' in every asset option)
+    context: __dirname,
+
+    // path to the clientlib root folder (output)
+    clientLibRoot: "./../ui.apps/src/main/content/jcr_root/apps/wknd-events/clientlibs",
+
+    libs: {
+        name: "angular-app",
+        allowProxy: true,
+        categories: ["wknd-events.angular"],
+        serializationFormat: "xml",
+        jsProcessor: ["default:none","min:none"],
+        assets: {
+            js: [
+                "dist/angular-app/**/*.js"
+            ],
+            css: [
+                "dist/angular-app/**/*.css"
+            ]
+        }
+    }
+};
