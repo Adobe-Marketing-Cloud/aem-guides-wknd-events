6a5f8ebfe709f206393fa956a63921327120ce8a
Samuel Meuli <meuli@adobe.com>
2019-12-10 09:51:27 +0100

    Document env variables

--- angular-app/src/environments/environment.prod.ts
+++ angular-app/src/environments/environment.prod.ts
@@ -1,5 +1,15 @@
 export const environment = {
+  // Root path of the AEM instance (used as the scope of the service worker,
+  // which needs to be able to intercept all requests to resources under that
+  // root path)
   aemRoot: '/',
+
+  // Equivalent of `process.env.NODE_ENV`
   production: true,
+
+  // Path to the `public` directory when the web app is deployed. In the case of
+  // AEM, that's the path to the `resources` ClientLib directory. Specifying
+  // this path is required for features like code splitting to work (because it
+  // will be prepended to paths in the `asset-manifest.json` file)
   publicUrl: '/etc.clientlibs/wknd-events/clientlibs/angular-app/resources'
 };
--- angular-app/src/environments/environment.ts
+++ angular-app/src/environments/environment.ts
@@ -1,18 +1,15 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
+  // Root path of the AEM instance (used as the scope of the service worker,
+  // which needs to be able to intercept all requests to resources under that
+  // root path)
   aemRoot: '/',
+
+  // Equivalent of `process.env.NODE_ENV`
   production: false,
+
+  // Path to the `public` directory when the web app is deployed. In the case of
+  // AEM, that's the path to the `resources` ClientLib directory. Specifying
+  // this path is required for features like code splitting to work (because it
+  // will be prepended to paths in the `asset-manifest.json` file)
   publicUrl: '/etc.clientlibs/wknd-events/clientlibs/angular-app/resources'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
